6262840751b8f06928a6069cd770f45e4b04ead8
troubleShooter239 <morgun2282@gmail.com>
2023-12-18 07:47:19 +0300

    Delete files

--- MVCWebApp/Controllers/AccountController.cs
+++ MVCWebApp/Controllers/AccountController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MVCWebApp.Controllers;
 
+
+[Authorize]
 public class AccountController : Controller
 {
     private readonly ILogger<AccountController> _logger;
@@ -11,9 +13,7 @@ public class AccountController : Controller
         _logger = logger;
     }
 
-    [Authorize]
     public IActionResult Cart() => View();
-    
-    [Authorize]
     public IActionResult Profile() => View();
+    public IActionResult OrderHistory() => View();
 }
